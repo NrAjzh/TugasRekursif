87b4c62fd21a2a6ae6cd4abdd789630a713c245c
NrAjzh <ajzh105@gmail.com>
2024-12-23 07:14:42 +0700

    Add files via upload

---
+++ NilaiMax.java
@@ -0,0 +1,19 @@
+public class NilaiMax {
+    public static int cariMax(int[] arr, int n) {
+        if 
+        (n == 1){
+            return arr [0];
+        }
+        int x = cariMax(arr, n - 1);
+
+        return Math.max(arr[n -1], x);
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {82,31,23,17,46,12,75};
+        int n = arr.length;
+
+        int max = cariMax(arr, n);
+        System.out.println("Nilai maksimum dalam array adalah: " + max);
+    }
+}
